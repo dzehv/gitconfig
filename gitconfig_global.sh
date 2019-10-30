4c3205514bca6d36fa24229bbbe102acb5c26864
Dmitriy Kapustin <dmitriy.kapustin@nurd.com>
2019-10-30 17:56:05 +0200

    example configuring gitlabs usage for 'go get'

--- gitconfig_global.sh
+++ gitconfig_global.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "\e[32mApplying settings...\e[0m"
+echo -e "\e[32mApplying settings...\e[0m"
 
-# Log alias
+# log alias
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
-# Color enabling settings
+# color enabling settings
 git config --global	color.diff true
 git config --global	color.status true
 git config --global	color.branch true
 git config --global	color.interactive true
 
-# Core settings
+# core settings
 git config --global	core.excludesfile ~/.gitexcludes
 git config --global core.ignorecase false
 
-### Default pull and push
+### default pull and push
 # upstream - push the current branch to its upstream branch...
 # simple - like upstream, but refuses to push if the upstream branch’s name is different from the local one...
 # current - push the current branch to a branch of the same name.
@@ -24,8 +24,11 @@ git config --global core.ignorecase false
 git config --global	pull.default upstream
 git config --global push.default upstream
 
-T_EDITOR=${EDITOR:=vim}
+T_EDITOR=${EDITOR:=emacs -nw}
 git config --global	core.editor "$T_EDITOR"
 
-# Print all settings
+# print all settings
 git --no-pager config --list
+
+# example configuring gitlabs usage for 'go get'
+# git config --global url."git@some.addr.com:".insteadOf "https://some.addr.com/"
